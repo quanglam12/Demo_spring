6472ea8d4432ebe684cd929ca41a5469e038e3a8
Quang Lâm <51247997+quanglam12@users.noreply.github.com>
2026-02-24 09:56:45 +0700

    change message return

--- src/main/java/com/example/demo_git/HelloController.java
+++ src/main/java/com/example/demo_git/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     public String sayHello() {
         int randomInt = (int)(Math.random() * 10);
         if(randomInt + 5 <= 10)
-            return "Hello world. Created test project.";
+            return "Hello world. Good afternoon.";
         else
             return "Good bye world!";
     }
