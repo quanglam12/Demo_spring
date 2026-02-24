7f9da2f764e8d29b85069982d5b8322dfcdcabff
Quang Lam <maivanquanglam@gmail.com>
2026-02-24 09:26:23 +0700

    add random number generation

---
+++ src/main/java/com/example/demo_git/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo_git;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        int randomInt = (int)(Math.random() * 10);
+        if(randomInt <= 5)
+            return "Hello world. Created test project.";
+        else
+            return "Good bye world!";
+    }
+}
